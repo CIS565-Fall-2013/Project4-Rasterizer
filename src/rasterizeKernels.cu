9d416f5d23fa92bde584e9d821316d19ab816480
nmarshak1337 <nmarshak@seas.upenn.edu>
2013-10-26 22:30:51 -0400

    Now the triangle outline draws more or less correctly. There is some annoying overshoot on triangle edges though.

--- src/rasterizeKernels.cu
+++ src/rasterizeKernels.cu
@@ -146,7 +146,7 @@ __device__ void writePointInTriangle(triangle currTri, glm::vec2 xyCoords, fragm
 	int pixX = roundf(xyCoords.x);
 	int pixY = roundf(xyCoords.y);
 	//TODO: incorporate the normal in here **somewhere**
-	writeToDepthbuffer(pixX, (resolution.y - 1) - pixY, currFrag, depthBuffer, resolution);
+	writeToDepthbuffer((resolution.x - 1) - pixX, (resolution.y - 1) - pixY, currFrag, depthBuffer, resolution);
 }
 
 //Based on slide 75-76 of the CIS560 notes, Norman I. Badler, University of Pennsylvania. 
@@ -227,9 +227,14 @@ __global__ void rasterizationKernel(triangle* primitives, int primitivesCount, f
   if(index<primitivesCount){
 	  //first rasterize the OUTLINES of the triangle
 	  triangle currTri = primitives[index];
-	  int numPixels = rasterizeLine(currTri.p0, currTri.p1, depthbuffer, resolution, currTri);
-	  numPixels += rasterizeLine(currTri.p1, currTri.p2, depthbuffer, resolution, currTri);
-	  numPixels += rasterizeLine(currTri.p2, currTri.p0, depthbuffer, resolution, currTri);
+	  //int numPixels = rasterizeLine(currTri.p0, currTri.p1, depthbuffer, resolution, currTri);
+	  //numPixels += rasterizeLine(currTri.p1, currTri.p2, depthbuffer, resolution, currTri);
+	  //numPixels += rasterizeLine(currTri.p2, currTri.p0, depthbuffer, resolution, currTri);
+	  int numPixels = rasterizeLine(currTri.p1, currTri.p0, depthbuffer, resolution, currTri);
+	  numPixels += rasterizeLine(currTri.p2, currTri.p1, depthbuffer, resolution, currTri);
+	  numPixels += rasterizeLine(currTri.p0, currTri.p2, depthbuffer, resolution, currTri);
+
+
 	  //use recursive flood fill starting at the CENTER of the triangle (interpolate using barycentric, map back to screen space)
 	  //take pixels, map them back to NDC, test to see if inside triangle (using barycentric)
 	  //i think the real speedup comes from backface culling - don't rasterize the triangle at all if the the winding order isn't facing the camera or something.
