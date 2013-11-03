77f53af7e7b3c892675537abebb3f912dc8ca10a
Lucy Xiao <yingtingx@gmail.com>
2013-11-02 20:28:47 -0400

    needs revert

--- src/rasterizeTools.h
+++ src/rasterizeTools.h
@@ -10,12 +10,9 @@
 #include "cudaMat4.h"
 
 struct triangle {
-  glm::vec3 p0; //the original vertices
+  glm::vec3 p0;
   glm::vec3 p1;
   glm::vec3 p2;
-  glm::vec3 pt0; //the transformed vertices
-  glm::vec3 pt1;
-  glm::vec3 pt2;
   glm::vec3 c0;
   glm::vec3 c1;
   glm::vec3 c2;
@@ -23,16 +20,15 @@ struct triangle {
   glm::vec3 n1;
   glm::vec3 n2;
 
-  __host__ __device__ triangle() : p0(), p1(), p2(), pt0(), pt1(), pt2(), c0(), c1(), c2(), n0(), n1(), n2() {};
+  __host__ __device__ triangle() : p0(), p1(), p2(), c0(), c1(), c2(), n0(), n1(), n2() {};
   __host__ __device__ triangle(glm::vec3 vp0, glm::vec3 vp1, glm::vec3 vp2, glm::vec3 vc0, glm::vec3 vc1, glm::vec3 vc2, glm::vec3 vn0, glm::vec3 vn1, glm::vec3 vn2) :
-	  p0(vp0), p1(vp1), p2(vp2), pt0(), pt1(), pt2(), c0(vc0), c1(vc1), c2(vc2), n0(vn0), n1(vn1), n2(vn2) {};
+	  p0(vp0), p1(vp1), p2(vp2), c0(vc0), c1(vc1), c2(vc2), n0(vn0), n1(vn1), n2(vn2) {};
 };
 
 struct fragment{
   glm::vec3 color;
   glm::vec3 normal;
   glm::vec3 position;
-  float z;
 };
 
 //Multiplies a cudaMat4 matrix and a vec4
@@ -46,30 +42,30 @@ __host__ __device__ glm::vec3 multiplyMV(cudaMat4 m, glm::vec4 v){
 
 //LOOK: finds the axis aligned bounding box for a given triangle
 __host__ __device__ void getAABBForTriangle(triangle tri, glm::vec3& minpoint, glm::vec3& maxpoint){
-  minpoint = glm::vec3(min(min(tri.pt0.x, tri.pt1.x),tri.pt2.x), 
-        min(min(tri.pt0.y, tri.pt1.y),tri.pt2.y),
-        min(min(tri.pt0.z, tri.pt1.z),tri.pt2.z));
-  maxpoint = glm::vec3(max(max(tri.pt0.x, tri.pt1.x),tri.pt2.x), 
-        max(max(tri.pt0.y, tri.pt1.y),tri.pt2.y),
-        max(max(tri.pt0.z, tri.pt1.z),tri.pt2.z));
+  minpoint = glm::vec3(min(min(tri.p0.x, tri.p1.x),tri.p2.x), 
+        min(min(tri.p0.y, tri.p1.y),tri.p2.y),
+        min(min(tri.p0.z, tri.p1.z),tri.p2.z));
+  maxpoint = glm::vec3(max(max(tri.p0.x, tri.p1.x),tri.p2.x), 
+        max(max(tri.p0.y, tri.p1.y),tri.p2.y),
+        max(max(tri.p0.z, tri.p1.z),tri.p2.z));
 }
 
 //LOOK: calculates the signed area of a given triangle
 __host__ __device__ float calculateSignedArea(triangle tri){
-  return 0.5*((tri.pt2.x - tri.pt0.x)*(tri.pt1.y - tri.pt0.y) - (tri.pt1.x - tri.pt0.x)*(tri.pt2.y - tri.pt0.y));
+  return 0.5*((tri.p2.x - tri.p0.x)*(tri.p1.y - tri.p0.y) - (tri.p1.x - tri.p0.x)*(tri.p2.y - tri.p0.y));
 }
 
 //LOOK: helper function for calculating barycentric coordinates
 __host__ __device__ float calculateBarycentricCoordinateValue(glm::vec2 a, glm::vec2 b, glm::vec2 c, triangle tri){
   triangle baryTri;
-  baryTri.pt0 = glm::vec3(a,0); baryTri.pt1 = glm::vec3(b,0); baryTri.pt2 = glm::vec3(c,0);
+  baryTri.p0 = glm::vec3(a,0); baryTri.p1 = glm::vec3(b,0); baryTri.p2 = glm::vec3(c,0);
   return calculateSignedArea(baryTri)/calculateSignedArea(tri);
 }
 
 //LOOK: calculates barycentric coordinates
 __host__ __device__ glm::vec3 calculateBarycentricCoordinate(triangle tri, glm::vec2 point){
-  float beta  = calculateBarycentricCoordinateValue(glm::vec2(tri.pt0.x,tri.pt0.y), point, glm::vec2(tri.pt2.x,tri.pt2.y), tri);
-  float gamma = calculateBarycentricCoordinateValue(glm::vec2(tri.pt0.x,tri.pt0.y), glm::vec2(tri.pt1.x,tri.pt1.y), point, tri);
+  float beta  = calculateBarycentricCoordinateValue(glm::vec2(tri.p0.x,tri.p0.y), point, glm::vec2(tri.p2.x,tri.p2.y), tri);
+  float gamma = calculateBarycentricCoordinateValue(glm::vec2(tri.p0.x,tri.p0.y), glm::vec2(tri.p1.x,tri.p1.y), point, tri);
   float alpha = 1.0-beta-gamma;
   return glm::vec3(alpha,beta,gamma);
 }
@@ -83,7 +79,7 @@ __host__ __device__ bool isBarycentricCoordInBounds(glm::vec3 barycentricCoord){
 
 //LOOK: for a given barycentric coordinate, return the corresponding z position on the triangle
 __host__ __device__ float getZAtCoordinate(glm::vec3 barycentricCoord, triangle tri){
-  return -(barycentricCoord.x*tri.pt0.z + barycentricCoord.y*tri.pt1.z + barycentricCoord.z*tri.pt2.z);
+  return -(barycentricCoord.x*tri.p0.z + barycentricCoord.y*tri.p1.z + barycentricCoord.z*tri.p2.z);
 }
 
 #endif
